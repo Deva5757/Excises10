353be7b1623963e428b7ea72988c3f933cc9cb50
Deva5757 <rd9786734602@gmail.com>
2023-06-25 18:46:21 +0530

    first message

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+function add(){
+    var a=parseInt(document.getElementById("num1").value);
+    var b=parseInt(document.getElementById("num2").value);
+    console.log(a+b);
+}
+
+function sub(){
+    var a=parseInt(document.getElementById("num1").value);
+    var b=parseInt(document.getElementById("num2").value);
+    console.log(a-b);
+}
+
+function mul(){
+    var a=parseInt(document.getElementById("num1").value);
+    var b=parseInt(document.getElementById("num2").value);
+    console.log(a*b);
+}
+
+function dived(){
+    var a=parseInt(document.getElementById("num1").value);
+    var b=parseInt(document.getElementById("num2").value);
+    console.log(a/b);
+}
+
+function mud(){
+    var a=parseInt(document.getElementById("num1").value);
+    var b=parseInt(document.getElementById("num2").value);
+    console.log(a%b);
+}
